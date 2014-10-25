631c86435a911f25796f1513504a8bb7ada4b598
Graeme Wolfendale <gwolfendale@gmail.com>
2014-10-25 02:18:07 +0100

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,4 +1,4 @@
-#333$(function(){ // on dom ready
+$(function(){ // on dom ready
   $('#cy').cytoscape({
     layout: {
       name: 'cose',
@@ -47,21 +47,21 @@
         }),
     elements: {
       nodes: [
-        { data: { id: 'j', name: 'Jerry', weight: 65, faveColor: '#333', faveShape: 'triangle' } },
-        { data: { id: 'e', name: 'Elaine', weight: 45, faveColor: '#333', faveShape: 'ellipse' } },
-        { data: { id: 'k', name: 'Kramer', weight: 75, faveColor: '#333', faveShape: 'octagon' } },
-        { data: { id: 'g', name: 'George', weight: 70, faveColor: '#333', faveShape: 'rectangle' } }
+        { data: { id: 'j', name: 'Jerry', weight: 65, faveColor: '#6FB1FC', faveShape: 'triangle' } },
+        { data: { id: 'e', name: 'Elaine', weight: 45, faveColor: '#EDA1ED', faveShape: 'ellipse' } },
+        { data: { id: 'k', name: 'Kramer', weight: 75, faveColor: '#86B342', faveShape: 'octagon' } },
+        { data: { id: 'g', name: 'George', weight: 70, faveColor: '#F5A45D', faveShape: 'rectangle' } }
       ],
       edges: [
-        { data: { source: 'j', target: 'e', faveColor: '#333', strength: 90 , name:'likes'} },
-        { data: { source: 'j', target: 'k', faveColor: '#333', strength: 70 , name:'likes'} },
-        { data: { source: 'j', target: 'g', faveColor: '#333', strength: 80 , name:'likes'} },
-        { data: { source: 'e', target: 'j', faveColor: '#333', strength: 95 , name:'likes'} },
-        { data: { source: 'e', target: 'k', faveColor: '#333', strength: 60, name:'likes' }, classes: 'questionable' },
-        { data: { source: 'k', target: 'j', faveColor: '#333', strength: 100, name:'likes' } },
-        { data: { source: 'k', target: 'e', faveColor: '#333', strength: 100 , name:'likes'} },
-        { data: { source: 'k', target: 'g', faveColor: '#333', strength: 100 , name:'likes'} },
-        { data: { source: 'g', target: 'j', faveColor: '#333', strength: 90 , name:'likes'} }
+        { data: { source: 'j', target: 'e', faveColor: '#6FB1FC', strength: 90 , name:'likes'} },
+        { data: { source: 'j', target: 'k', faveColor: '#6FB1FC', strength: 70 , name:'likes'} },
+        { data: { source: 'j', target: 'g', faveColor: '#6FB1FC', strength: 80 , name:'likes'} },
+        { data: { source: 'e', target: 'j', faveColor: '#EDA1ED', strength: 95 , name:'likes'} },
+        { data: { source: 'e', target: 'k', faveColor: '#EDA1ED', strength: 60, name:'likes' }, classes: 'questionable' },
+        { data: { source: 'k', target: 'j', faveColor: '#86B342', strength: 100, name:'likes' } },
+        { data: { source: 'k', target: 'e', faveColor: '#86B342', strength: 100 , name:'likes'} },
+        { data: { source: 'k', target: 'g', faveColor: '#86B342', strength: 100 , name:'likes'} },
+        { data: { source: 'g', target: 'j', faveColor: '#F5A45D', strength: 90 , name:'likes'} }
       ]
     }, 
     ready: function(){
